80eaa899de51df5d13bf73d3dafea20cebb00ca3
Matthias Bernouy <matt.bernouy@orange.fr>
2023-12-12 14:21:46 +0100

    Matthias Metier Update

--- src/metier/repo/ModuleDB.java
+++ src/metier/repo/ModuleDB.java
@@ -14,12 +14,18 @@ public class ModuleDB {
 
     private Connection db = DB.getInstance();
 
-    private PreparedStatement psGetModules;
+    private SemestreDB semestreDB;
+    private CategorieModuleDB categorieModuleDB;
 
+    private PreparedStatement psGetModules;
+    private PreparedStatement psGetModuleParCode;
 
     public ModuleDB(){
+        this.semestreDB = new SemestreDB();
+        this.categorieModuleDB = new CategorieModuleDB();
         try{
             this.psGetModules = db.prepareStatement("SELECT * FROM Module");
+            this.psGetModuleParCode = db.prepareStatement("SELECT * FROM Module WHERE code = ?")
         } catch ( Exception e ){
             e.printStackTrace();
         }
@@ -28,15 +34,28 @@ public class ModuleDB {
     public List<Module> getModules(){
         DBResult result = DB.query(this.psGetModules);
         List<Module> modules = new ArrayList<>();
-        for ( Map<String, String> ligne : result.getLignes() ){
-            modules.add(new Module(
-                    Integer.parseInt(ligne.get("idModule")),
-                    ligne.get("nomModule"),
-                    ligne.get("idCatModule"),
-                    Integer.parseInt(ligne.get("nbHeures"))
-            ));
-        }
+        for ( Map<String, String> ligne : result.getLignes() )
+            modules.add(ligneToModule(ligne));
         return modules;
+    }
+
+    public Module getModuleParCode(String code){
+        try{ this.psGetModuleParCode.setString(1, code); }
+        catch ( Exception e ) { e.printStackTrace(); }
+        DBResult result = DB.query(this.psGetModuleParCode);
+        return ligneToModule(result.getLignes().get(0));
+    }
+
+    private Module ligneToModule(Map<String, String> ligne){
+        return new Module(
+                ligne.get("code"),
+                semestreDB.getSemestreParId(Integer.parseInt(ligne.get("idSemestre"))),
+                categorieModuleDB.getCategorieModuleParId(ligne.get("idCatModule")),
+                Boolean.getBoolean(ligne.get("forceValider")),
+                ligne.get("libLong"),
+                ligne.get("libCourt"),
+                null
+        );
     }
 
     /*
