77ef90c0f25c4c17c2b9b55ce41f553c88e51980
Dunet Tom <116595007+Oridoshi@users.noreply.github.com>
2023-12-13 09:00:38 +0100

    push controleur Axelito

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -32,4 +32,33 @@ public class Controleur
 	{
 		return new Semestre(1, 50, 60, 92, 14);
 	}
+
+	public ArrayList<Intervenant> getLstIntervenants()
+	{
+		ArrayList<Intervenant> lstIntervenants = new ArrayList<Intervenant>();
+		
+
+		return lstIntervenants;
+	}
+
+
+	public ArrayList<CategorieIntervenant> getLstCategorieIntervenant()
+	{
+		ArrayList<CategorieIntervenant> lstCategorieIntervenant = new ArrayList<CategorieIntervenant>();
+		lstCategorieIntervenant.add(new CategorieIntervenant("type_prof", "Professeur", 192, 384, 1));
+		lstCategorieIntervenant.add(new CategorieIntervenant("type_vac", "Vacataire", 124, 258, 1));
+
+		return lstCategorieIntervenant;
+	}
+
+	public ArrayList<CategorieHeure> getLstCategorieHeure()
+	{
+		ArrayList<CategorieHeure> lstCategorieHeure = new ArrayList<CategorieHeure>();
+
+		lstCategorieHeure.add(new CategorieHeure("TD", 1));
+		lstCategorieHeure.add(new CategorieHeure("TP", 2/3));
+		lstCategorieHeure.add(new CategorieHeure("CM", 1));
+
+		return lstCategorieHeure;
+	}
 }
